6abd1041f31cd95694fed64f4febddd8be0b56e2
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-07-01 22:12:30 +0600

    Documentation #2 Text change in activity_main.xml file
    Changed text from "Hello World!" to "Happy Birthday to Sazia"

--- Lab/Lab01/app/src/main/java/com/example/sadia/happybirthday/MainActivity.java
+++ Lab/Lab01/app/src/main/java/com/example/sadia/happybirthday/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.sadia.happybirthday;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
     }
 }
