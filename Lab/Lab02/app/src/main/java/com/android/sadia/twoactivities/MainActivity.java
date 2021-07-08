07a3ba7ae7fe1a096b8b683f21b0dace38ae28de
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-07-09 00:24:55 +0600

    Documentation #4 Generate SrollView Text
    Added switch case to output scroll texts according to cases

--- Lab/Lab02/app/src/main/java/com/android/sadia/twoactivities/MainActivity.java
+++ Lab/Lab02/app/src/main/java/com/android/sadia/twoactivities/MainActivity.java
@@ -7,26 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public Intent intent;
+    public static final String CASES =
+            "com.android.sadia.twoactivities.CASES";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, SecondActivity.class);
     }
 
     public void launchSecondActivityOne(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("CASES","A");
         startActivity(intent);
     }
 
     public void launchSecondActivityThree(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra ("CASES","C");
         startActivity(intent);
     }
 
     public void launchSecondActivityTwo(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        intent = this.intent.putExtra("CASES", "B");
+        startActivity(this.intent);
 
     }
 }
--- Lab/Lab02/app/src/main/java/com/android/sadia/twoactivities/SecondActivity.java
+++ Lab/Lab02/app/src/main/java/com/android/sadia/twoactivities/SecondActivity.java
@@ -2,13 +2,35 @@ package com.android.sadia.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+        TextView scroll_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        scroll_textView = (TextView) findViewById(R.id.scroll_text);
+         switch(intent.getStringExtra( MainActivity.CASES)){
+
+             case "A":
+                 scroll_textView.setText(R.string.lorem1) ;
+                 break;
+
+             case "B":
+                 scroll_textView.setText(R.string.lorem2) ;
+                 break;
+
+             case "C":
+                 scroll_textView. setText(R.string.lorem3) ;
+                 break;
+
+             default:
+                 throw new IllegalStateException("Unexpected value: " + intent.getStringExtra(MainActivity.CASES));
+         }
     }
 }
