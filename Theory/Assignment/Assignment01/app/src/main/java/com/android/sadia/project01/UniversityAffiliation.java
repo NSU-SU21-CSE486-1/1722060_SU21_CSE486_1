91d59d053676755e8a6202d28faa3ba2cf42a136
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-07-29 03:03:42 +0600

    bug enhancement #8 New activity created,  made changes to .xml file
    Added input fields to University Affiliation activity and passed values into it

--- Theory/Assignment/Assignment01/app/src/main/java/com/android/sadia/project01/UniversityAffiliation.java
+++ Theory/Assignment/Assignment01/app/src/main/java/com/android/sadia/project01/UniversityAffiliation.java
@@ -2,11 +2,21 @@ package com.android.sadia.project01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class UniversityAffiliation extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE =
+            "com.android.sadia.project01.extra.MESSAGE";
+
+    private EditText uniNameEditText;
+    private EditText departmentEditText;
+    private EditText studentIdEditText;
+    private EditText studyLevelEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +42,30 @@ public class UniversityAffiliation extends AppCompatActivity {
         String blood_group_message = intent3.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView3 = findViewById(R.id.blood_group_field);
         textView3.setText(blood_group_message);
+
+        uniNameEditText = findViewById(R.id.input_university_name);
+        departmentEditText = findViewById(R.id.input_department);
+        studentIdEditText = findViewById(R.id.input_student_id);
+        studyLevelEditText = findViewById(R.id.input_study_level);
+    }
+
+    public void launchStudentInformation(View view) {
+        Intent intent = new Intent(this, StudentInformation.class);
+        String uniName_message = uniNameEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, uniName_message);
+        startActivity(intent);
+
+        String department_message = departmentEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, department_message);
+        startActivity(intent);
+
+        String studentId_message = studentIdEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, studentId_message);
+        startActivity(intent);
+
+        String studyLevel_message = studyLevelEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, studyLevel_message);
+        startActivity(intent);
+
     }
 }
