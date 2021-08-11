e673f259142a981a988e8b251fbc3f5f10d14295
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-08-12 01:56:58 +0600

    #10 Update OrderActivity.java
    Updated OrderActivity .java so that the new activity shows the toast message

--- Practice/Practice001/app/src/main/java/com/android/sadia/droidcafe/MainActivity.java
+++ Practice/Practice001/app/src/main/java/com/android/sadia/droidcafe/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String mOrderMessage;
+    public static final String EXTRA_MESSAGE =
+            "com.android.sadia.droidcafe.extra.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,17 +24,21 @@ public class MainActivity extends AppCompatActivity {
                 Toast.LENGTH_SHORT).show();
     }
     public void showDonutOrder(View view) {
-        displayToast(getString(R.string.donut_order_message));
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
     }
     public void showIceCreamOrder(View view) {
-        displayToast(getString(R.string.ice_cream_order_message));
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
     }
     public void showFroyoOrder(View view) {
-        displayToast(getString(R.string.froyo_order_message));
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
         startActivity(intent);
     }
 
