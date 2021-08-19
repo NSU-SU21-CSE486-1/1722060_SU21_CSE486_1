a29d1eee69cc7931c2ce80378a7423fb8618b83e
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-08-20 03:35:36 +0600

    #12 Register and Unregister Receiver
    Registered a receiver, Implemented the receiver in a different class, and unregistered it

---
+++ Lab/Lab05/app/src/main/java/com/android/sadia/connectedordisconnected/CustomReceiver.java
@@ -0,0 +1,36 @@
+package com.android.sadia.connectedordisconnected;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+    boolean Headset_Plugged_in = false;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+        int extraMessage;
+
+        if (Intent.ACTION_HEADSET_PLUG.equals(intentAction)) {
+            extraMessage = intent.getIntExtra("state", -1);
+            String toastMessage = "unknown intent action";
+
+            switch(extraMessage){
+                case 0:
+                    Headset_Plugged_in = false;
+                    toastMessage = "Headset Disconnected!";
+                    break;
+
+                case 1:
+                    Headset_Plugged_in = true;
+                    toastMessage = "Headset Connected!";
+                    break;
+            }
+
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
--- Lab/Lab05/app/src/main/java/com/android/sadia/connectedordisconnected/MainActivity.java
+++ Lab/Lab05/app/src/main/java/com/android/sadia/connectedordisconnected/MainActivity.java
@@ -2,13 +2,28 @@ package com.android.sadia.connectedordisconnected;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final CustomReceiver mReceiver = new CustomReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+        this.registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
