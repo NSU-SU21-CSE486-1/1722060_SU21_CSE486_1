d908b0cbeb6f1a0a20eac8685a835b6036505d65
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-09-06 21:54:20 +0600

    bug #14 Update Buttons and Spinners
    Implemented spinner, and toggle methods

--- Lab/Lab07/app/src/main/java/com/android/sadia/hellosharedprefs/Settings.java
+++ Lab/Lab07/app/src/main/java/com/android/sadia/hellosharedprefs/Settings.java
@@ -4,22 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.SharedPreferences;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
 public class Settings extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener{
+
+    private int mCount = 0;
+    private int mColor;
+
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+
+    private TextView mShowCountTextView;
+
+    private SharedPreferences mPreferences;
+
+    private String sharedPrefFile = "com.example.android.hellosharedprefs";
+
+    private SwitchMaterial switch1;
+    private SwitchMaterial switch2;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
+        mCount = mPreferences.getInt(COUNT_KEY, 0);
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+        mShowCountTextView.setBackgroundColor(mColor);
 
         Spinner spinner = findViewById(R.id.color_spinner);
         if (spinner != null) {
@@ -35,7 +71,50 @@ public class Settings extends AppCompatActivity implements
             spinner.setAdapter(adapter);
         }
 
+        switch1 = findViewById(R.id.toggle_save);
+
+
+        switch2 = findViewById(R.id.toggle_reset);
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean checked) {
+                if(checked){
+                    reset();
+                }
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt(COUNT_KEY, mCount);
+        preferencesEditor.putInt(COLOR_KEY, mColor);
+        preferencesEditor.apply();
+    }
+
+
+    public void reset() {
+        // Reset count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        // Reset color
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
+
+        // Clear preferences
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.clear();
+        preferencesEditor.apply();
+    }
+
+
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
