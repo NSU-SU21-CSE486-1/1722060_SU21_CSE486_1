83740cd0c5b4991558c308e375c6fc0b644231b0
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-09-07 01:18:30 +0600

    #14 Update Settings activity, and Switch
    Deleted irrelevant methods, and Switch text

--- Lab/Lab07/app/src/main/java/com/android/sadia/hellosharedprefs/Settings.java
+++ Lab/Lab07/app/src/main/java/com/android/sadia/hellosharedprefs/Settings.java
@@ -19,9 +19,6 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 public class Settings extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener{
 
-    private int mCount = 0;
-    private int mColor;
-
 
     // Key for current count
     private final String COUNT_KEY = "count";
@@ -31,8 +28,6 @@ public class Settings extends AppCompatActivity implements
     private TextView mShowCountTextView;
 
     private SharedPreferences mPreferences;
-
-    private String sharedPrefFile = "com.example.android.hellosharedprefs";
 
     private SwitchMaterial switch1;
     private SwitchMaterial switch2;
@@ -45,17 +40,6 @@ public class Settings extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        mShowCountTextView = findViewById(R.id.count_textview);
-        mColor = ContextCompat.getColor(this,
-                R.color.default_background);
-
-        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
-
-        mCount = mPreferences.getInt(COUNT_KEY, 0);
-        mShowCountTextView.setText(String.format("%s", mCount));
-
-        mColor = mPreferences.getInt(COLOR_KEY, mColor);
-        mShowCountTextView.setBackgroundColor(mColor);
 
         Spinner spinner = findViewById(R.id.color_spinner);
         if (spinner != null) {
@@ -72,46 +56,8 @@ public class Settings extends AppCompatActivity implements
         }
 
         switch1 = findViewById(R.id.toggle_save);
-
-
         switch2 = findViewById(R.id.toggle_reset);
-        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean checked) {
-                if(checked){
-                    reset();
-                }
-            }
-        });
 
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
-        preferencesEditor.putInt(COUNT_KEY, mCount);
-        preferencesEditor.putInt(COLOR_KEY, mColor);
-        preferencesEditor.apply();
-    }
-
-
-    public void reset() {
-        // Reset count
-        mCount = 0;
-        mShowCountTextView.setText(String.format("%s", mCount));
-
-        // Reset color
-        mColor = ContextCompat.getColor(this,
-                R.color.default_background);
-        mShowCountTextView.setBackgroundColor(mColor);
-
-        // Clear preferences
-        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
-        preferencesEditor.clear();
-        preferencesEditor.apply();
     }
 
 
