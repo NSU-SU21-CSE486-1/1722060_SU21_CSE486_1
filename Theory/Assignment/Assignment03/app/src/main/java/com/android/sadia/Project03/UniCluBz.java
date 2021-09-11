a729637252a2abea15769f10c868263ca0059283
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-09-11 06:19:29 +0600

    #15 Add ViewModel to Activity Lifecycle
    Added "uniCluBzListViewModel" to the UniCluBz Activity

--- Theory/Assignment/Assignment03/app/src/main/java/com/android/sadia/Project03/UniCluBz.java
+++ Theory/Assignment/Assignment03/app/src/main/java/com/android/sadia/Project03/UniCluBz.java
@@ -1,13 +1,20 @@
 package com.android.sadia.Project03;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.List;
 
 public class UniCluBz extends AppCompatActivity {
 
@@ -15,6 +22,9 @@ public class UniCluBz extends AppCompatActivity {
     TabItem tabItem1,tabItem2,tabItem3;
     ViewPager viewPager;
     PageAdapter pageAdapter;
+    private FragmentUniversityAffiliation fragmentA;
+    private ftab2 fragmentB;
+    private UniCluBzListViewModel uniCluBzListViewModel;
 
 
     @Override
@@ -22,11 +32,26 @@ public class UniCluBz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uniclubz);
 
+        uniCluBzListViewModel = ViewModelProviders.of(this).get(UniCluBzListViewModel.class);
+        uniCluBzListViewModel.getAllUniCluBzLists().observe(this, new Observer<List<UniCluBzList>>() {
+            @Override
+            public void onChanged(List<UniCluBzList> uniCluBzLists) {
+                
+                Toast toast = Toast.makeText(UniCluBz.this, "On Changed", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+
         tabLayout = (TabLayout) findViewById(R.id.tablayout1);
         tabItem1 = (TabItem) findViewById(R.id.tab1);
         tabItem2 = (TabItem) findViewById(R.id.tab2);
         tabItem3 = (TabItem) findViewById(R.id.tab3);
         viewPager = (ViewPager) findViewById(R.id.vpager);
+        fragmentA = new FragmentUniversityAffiliation();
+        fragmentB = new ftab2();
+
+
 
         pageAdapter = new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
         viewPager.setAdapter(pageAdapter);
@@ -53,5 +78,6 @@ public class UniCluBz extends AppCompatActivity {
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
+
 
 }
