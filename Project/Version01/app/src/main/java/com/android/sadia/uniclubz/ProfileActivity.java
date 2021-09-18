716c3be38aba61b0e5bea4c82513e23c05a79b61
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-09-18 17:36:32 +0600

    #17 User Profile page Setup
    Created a new Profile Activity, and added a Logout button, wrote code for signing out users from the system

---
+++ Project/Version01/app/src/main/java/com/android/sadia/uniclubz/ProfileActivity.java
@@ -0,0 +1,31 @@
+package com.android.sadia.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Button logoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logoutButton = findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+            }
+        });
+
+    }
+}
