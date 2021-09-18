9d7f7c35782f6f559c2a41794b0b0f8d16103b34
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-09-18 15:01:58 +0600

    #17 Update Login Page
    Added onClick method to Login Activity

--- Project/Version01/app/src/main/java/com/android/sadia/uniclubz/MainActivity.java
+++ Project/Version01/app/src/main/java/com/android/sadia/uniclubz/MainActivity.java
@@ -2,13 +2,30 @@ package com.android.sadia.uniclubz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup = findViewById(R.id.sign_up);
+        signup.setOnClickListener(this);
     }
-}
+
+        @Override
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.sign_up:
+                    startActivity(new Intent(this,SignupActivity.class));
+                    break;
+            }
+
+        }
+} 
