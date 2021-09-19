7bbb9c0f53ffaf7fd51c0f264117b6a38c8033d2
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-09-19 14:49:04 +0600

    #18 Create Variable and Initialize
    Created a member variable for the button, initialized the variable in the onCreate() method

--- Lab/Lab08/app/src/main/java/com/android/sadia/notifyme/MainActivity.java
+++ Lab/Lab08/app/src/main/java/com/android/sadia/notifyme/MainActivity.java
@@ -3,12 +3,16 @@ package com.android.sadia.notifyme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button_notify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
     }
 }
