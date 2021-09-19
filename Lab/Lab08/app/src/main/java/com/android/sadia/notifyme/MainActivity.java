eaaac0d50f580f0e330df31eacc3afefbf25d682
SadiaSheikh <sumiya.sadia@northsouth.edu>
2021-09-19 14:50:40 +0600

    #18 Add onClickListener
    Created an onClickListener() method for the button

--- Lab/Lab08/app/src/main/java/com/android/sadia/notifyme/MainActivity.java
+++ Lab/Lab08/app/src/main/java/com/android/sadia/notifyme/MainActivity.java
@@ -3,6 +3,7 @@ package com.android.sadia.notifyme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
     }
 }
